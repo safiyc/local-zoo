df38ea78757ee9d3c1f2ab653295177738acbfb7
Safiy Cham <csafi13@yahoo.com>
2017-10-27 08:38:26 -0700

    add initial files.

---
+++ README.md
@@ -0,0 +1,7 @@
+create: resources/styles/styles.css
+
+npm install
+bower install
+bower install bootstrap --save
+gulp build
+gulp serve
---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Angular 2 App is Working</h1>
+  `
+})
+
+export class AppComponent {
+
+}

71611747110462b9f5fa59bf04bc43254c21aeee
Safiy Cham <csafi13@yahoo.com>
2017-10-27 09:27:23 -0700

    add Animal class with properties.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Angular 2 App is Working</h1>
+  <div class="container">
+    <h1>{{title}}</h1>
+  </div>
   `
 })
 
 export class AppComponent {
-
+  title = 'Zoo\'s Animal Tracking';
 }
